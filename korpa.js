cdd8389e3aa0512db33e88afb8e96d7ef20cad22
Goran023 <023gmk@gmail.com>
2019-02-08 20:23:52 +0100

    korpa u odvojenom js fajlu ...

---
+++ korpa.js
@@ -0,0 +1,23 @@
+function start() {
+    var r = 0;
+    var total = 0;
+    var red = localStorage.getItem("red");
+    var rod = red.split(",");
+    if (rod.length < 6) {
+        alert("Prazna korpa");
+    }
+    else {
+        for (x = 0; x < rod.length; x = x + 6) {
+            var trow = document.createElement("tr");
+            document.getElementById("pText").appendChild(trow);
+            for (i = 0; i < 6; i = i + 1) {
+                if (i == 5) total = total + parseFloat(rod[i + r]);
+                var tdat = document.createElement("td");
+                tdat.innerHTML = rod[i + r];
+                document.getElementById("pText").appendChild(tdat);
+            }
+            r = r + 6;
+        }
+    }
+    document.getElementById("total").innerHTML = 'postovani <b>IME</b> i <b>PREZIME</b>, vas ukupan racun je <b>' + total.toFixed(2) + '</b> RSD';
+}
