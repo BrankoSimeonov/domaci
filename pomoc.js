e4a9bdeb4091e0e859aa8f0dcbc11c8048bb6b29
MilatovicV <milatovicverica@gmail.com>
2019-02-11 20:55:13 +0100

    JS za stranicu pomoc

---
+++ pomoc.js
@@ -0,0 +1,19 @@
+//the modal
+var modal = document.getElementById('myModal');
+                                    
+var img = document.getElementById('uplatnica');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}
